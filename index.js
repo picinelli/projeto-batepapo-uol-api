4fa3cab4d5f6e586d382cdedf62c22fe7186bb64
Pedro Ivo <cinelli.dev@gmail.com>
2022-04-28 14:00:30 -0300

    feat - MongoDB integrated & status verification

--- index.js
+++ index.js
@@ -2,110 +2,217 @@ import express from "express";
 import cors from "cors";
 import Joi from "joi";
 import dayjs from "dayjs";
+import { MongoClient } from "mongodb";
+import "dotenv/config";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const users = [];
-const messages = [];
+const mongoClient = new MongoClient(process.env.MONGO_URI); // USAR O ENV !!!!!!!!!!!!!!!!!!!
+let db;
 
-app.post("/participants", (req, res) => {
+const promise = mongoClient.connect().then(() => {
+  db = mongoClient.db("projeto_batepapo_api");
+});
+
+app.post("/participants", async (req, res) => {
   const { name } = req.body;
-  const userAlreadyExists = users.find((user) => user.name === name);
-  const schema = Joi.object({
-    name: Joi.string().required(),
-  });
-  const { error, value } = schema.validate({ name: name });
-
-  if (userAlreadyExists) {
-    res.status(409).send("User already exists");
-    return
-  } else if (error === undefined) {
-    const now = Date.now();
-    users.push({ name: value.name, lastStatus: now });
-    messages.push({
-      from: value.name,
-      to: "Todos",
-      text: "entra na sala...",
-      type: "status",
-      time: dayjs(now).format("HH:mm:ss"),
+  try {
+    mongoClient.connect()
+    const participants = await db.collection("users").find({}).toArray();
+    const userAlreadyExists = participants.find((user) => user.name === name);
+    const schema = Joi.object({
+      name: Joi.string().required(),
     });
+    const { error, value } = schema.validate({ name: name });
+    if (userAlreadyExists) {
+      res.status(409).send("User already exists");
+      mongoClient.close()
+      return;
+    } else if (error === undefined) {
+      const now = Date.now();
+      try {
+        await db
+          .collection("users")
+          .insertOne({ name: value.name, lastStatus: now });
+        try {
+          await db.collection("messages").insertOne({
+            from: value.name,
+            to: "Todos",
+            text: "entra na sala...",
+            type: "status",
+            time: dayjs(now).format("HH:mm:ss"),
+          });
+          res.sendStatus(201);
+        } catch (e) {
+          mongoClient.close()
+          console.error(e);
+        }
+      } catch (e) {
+        mongoClient.close()
+        console.error(e);
+      }
+      return;
+    } else {
+      mongoClient.close()
+      res.status(422).send(error);
+      return;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+  mongoClient.close()
+});
 
-    res.status(201).send();
-    return
-  } else {
-    res.status(422).send(error);
-    return
+app.get("/participants", async (req, res) => {
+  try {
+    ;
+    const participants = await db.collection("users").find({}).toArray();
+    res.status(200).send(participants);
+    ;
+    return;
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+    ;
+    return;
   }
 });
 
-app.get("/participants", (req, res) => {
-  res.status(200).send(users);
-});
-
-app.post("/messages", (req, res) => {
+app.post("/messages", async (req, res) => {
   const now = Date.now();
   const nowFormattted = dayjs(now).format("HH:mm:ss");
   const body = req.body;
   const thisUser = req.headers.user;
   const newMessage = { from: thisUser, ...body, time: nowFormattted };
-  const userIsOnline = users.find((user) => user.name === thisUser);
 
-  const schema = Joi.object({
-    to: Joi.string().required(),
-    text: Joi.string().required(),
-    type: Joi.any().valid("message", "private_message"),
-    time: Joi.any(),
-    from: Joi.any().required(),
-  });
-  const { error, value } = schema.validate({
-    ...newMessage,
-    from: userIsOnline,
-  });
+  try {
+    const users = await db.collection("users").find({}).toArray();
+    const userIsOnline = users.find((user) => user.name === thisUser);
+    const schema = Joi.object({
+      to: Joi.string().required(),
+      text: Joi.string().required(),
+      type: Joi.any().valid("message", "private_message"),
+      time: Joi.any(),
+      from: Joi.any().required(),
+    });
+    const { error, value } = schema.validate({
+      ...newMessage,
+      from: userIsOnline,
+    });
 
-  if (error === undefined) {
-    messages.push(newMessage)
-    res.sendStatus(201);
-    return
-  } else {
-    res.status(422).send(error);
+    if (error === undefined) {
+      try {
+        await db.collection("messages").insertOne(newMessage);
+        res.sendStatus(201);
+      } catch (e) {
+        console.error(e);
+      }
+      return;
+    } else {
+      res.status(422).send(error);
+      return;
+    }
+  } catch (e) {
+    console.error(e);
+    return;
+  }
+});
+
+app.get("/messages", async (req, res) => {
+  const user = req.headers.user;
+  const messagesAmount = parseInt(req.query.limit);
+  try {
+    const messages = await db.collection("messages").find({}).toArray();
+    const haveAccessMessages = messages.filter((message) => {
+      if (
+        user === message.to ||
+        user === message.from ||
+        message.to === "Todos"
+      ) {
+        return message;
+      }
+    });
+    const messagesSliced = haveAccessMessages.slice(-messagesAmount);
+    res.status(200).send(messagesSliced);
+    return;
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+    return;
+  }
+});
+
+app.post("/status", async (req, res) => {
+  const userHeader = req.headers.user;
+
+  try {
+    const users = await db.collection("users").find({}).toArray();
+    const userIsOnline = users.some((user) => user.name === userHeader);
+    if (!userIsOnline) {
+      res.sendStatus(404);
+      return;
+    } else {
+      const now = Date.now();
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].name === userHeader) {
+          try {
+            await db.collection("users").updateOne({name: userHeader},
+              {$set: {
+                lastStatus: now
+              }})
+            break;
+          } catch (e) {
+            console.error(e);
+          }
+        }
+      }
+      res.sendStatus(200);
+      return;
+    }
+  } catch (e) {
+    console.error(e);
     return
   }
 });
 
-app.get("/messages", (req, res) => {
-  const user = req.headers.user
-  const messagesAmount = parseInt(req.query.limit)
-  const haveAccessMessages = messages.filter((message) => {
-    if(user === message.to || user === message.from || message.to === "Todos") {
-      return message
-    } 
-  })
-  const messagesSliced = haveAccessMessages.slice(-messagesAmount)
+setInterval(async () => {
+  const now = Date.now()
 
-  res.status(200).send(messagesSliced)
-})
-
-app.post("/status", (req, res) => {
-  const userHeader = req.headers.user
-  const userIsOnline = users.some(user => user.name === userHeader)
-
-  if(!userIsOnline) {
-    res.sendStatus(404)
-    return
-  } else {
-    const now = Date.now();
-    for(let i = 0; i < users.length; i++) {
-      if(users[i].name === userHeader) {
-        users[i] = {name: userHeader, lastStatus: now}
-        break
-      }
+  try {
+    const users = await db.collection("users").find({}).toArray();
+    const disconnectedUsers = users.filter((user) => {
+      if (now - user.lastStatus > 10000) return user;
+    });
+    try {
+      disconnectedUsers.map(async (user) => {
+        await db.collection("users").deleteOne(user);
+        try {
+          await db
+          .collection("messages")
+          .insertOne({
+            from: user.name,
+            to: "Todos",
+            text: "sai da sala...",
+            type: "status",
+            time: dayjs(now).format("HH:mm:ss"),
+          });
+          return
+        } catch(e) {
+          console.error(e);
+          return
+        }
+      });
+    } catch (e) {
+      console.error(e);
+      return
     }
-    res.sendStatus(200)
+  } catch (e) {
+    console.error(e);
     return
   }
-})
+}, 15000);
 
 app.listen(5000, () => {
   console.log("Servidor Iniciado!");
