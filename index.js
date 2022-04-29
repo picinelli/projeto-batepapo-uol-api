978947dbce608f86280ec7edb7e42cb730267b8b
Pedro Ivo <cinelli.dev@gmail.com>
2022-04-29 10:46:07 -0300

    fix - unnecessary  try/catch chaining removed

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const promise = mongoClient.connect().then(() => {
 app.post("/participants", async (req, res) => {
   const { name } = req.body;
   try {
-    mongoClient.connect()
+    
     const participants = await db.collection("users").find({}).toArray();
     const userAlreadyExists = participants.find((user) => user.name === name);
     const schema = Joi.object({
@@ -28,54 +28,47 @@ app.post("/participants", async (req, res) => {
     const { error, value } = schema.validate({ name: name });
     if (userAlreadyExists) {
       res.status(409).send("User already exists");
-      mongoClient.close()
+      
       return;
     } else if (error === undefined) {
       const now = Date.now();
-      try {
-        await db
-          .collection("users")
-          .insertOne({ name: value.name, lastStatus: now });
-        try {
-          await db.collection("messages").insertOne({
-            from: value.name,
-            to: "Todos",
-            text: "entra na sala...",
-            type: "status",
-            time: dayjs(now).format("HH:mm:ss"),
-          });
-          res.sendStatus(201);
-        } catch (e) {
-          mongoClient.close()
-          console.error(e);
-        }
-      } catch (e) {
-        mongoClient.close()
-        console.error(e);
-      }
+
+      await db
+        .collection("users")
+        .insertOne({ name: value.name, lastStatus: now });
+      await db.collection("messages").insertOne({
+        from: value.name,
+        to: "Todos",
+        text: "entra na sala...",
+        type: "status",
+        time: dayjs(now).format("HH:mm:ss"),
+      });
+
+      res.sendStatus(201);
+      
       return;
     } else {
-      mongoClient.close()
+      
       res.status(422).send(error);
       return;
     }
   } catch (e) {
     console.error(e);
   }
-  mongoClient.close()
+  
 });
 
 app.get("/participants", async (req, res) => {
   try {
-    ;
+    
     const participants = await db.collection("users").find({}).toArray();
     res.status(200).send(participants);
-    ;
+    
     return;
   } catch (e) {
     console.error(e);
     res.sendStatus(500);
-    ;
+    
     return;
   }
 });
@@ -88,6 +81,7 @@ app.post("/messages", async (req, res) => {
   const newMessage = { from: thisUser, ...body, time: nowFormattted };
 
   try {
+    
     const users = await db.collection("users").find({}).toArray();
     const userIsOnline = users.find((user) => user.name === thisUser);
     const schema = Joi.object({
@@ -103,27 +97,28 @@ app.post("/messages", async (req, res) => {
     });
 
     if (error === undefined) {
-      try {
-        await db.collection("messages").insertOne(newMessage);
-        res.sendStatus(201);
-      } catch (e) {
-        console.error(e);
-      }
+      await db.collection("messages").insertOne(newMessage);
+      res.sendStatus(201);
+      
       return;
     } else {
       res.status(422).send(error);
+      
       return;
     }
   } catch (e) {
     console.error(e);
+    
     return;
   }
 });
 
+// nao esta retornando as mais recentes.
 app.get("/messages", async (req, res) => {
   const user = req.headers.user;
   const messagesAmount = parseInt(req.query.limit);
   try {
+    
     const messages = await db.collection("messages").find({}).toArray();
     const haveAccessMessages = messages.filter((message) => {
       if (
@@ -136,10 +131,12 @@ app.get("/messages", async (req, res) => {
     });
     const messagesSliced = haveAccessMessages.slice(-messagesAmount);
     res.status(200).send(messagesSliced);
+    
     return;
   } catch (e) {
     console.error(e);
     res.sendStatus(500);
+    
     return;
   }
 });
@@ -148,69 +145,64 @@ app.post("/status", async (req, res) => {
   const userHeader = req.headers.user;
 
   try {
+    
     const users = await db.collection("users").find({}).toArray();
     const userIsOnline = users.some((user) => user.name === userHeader);
     if (!userIsOnline) {
       res.sendStatus(404);
+      
       return;
     } else {
       const now = Date.now();
       for (let i = 0; i < users.length; i++) {
         if (users[i].name === userHeader) {
-          try {
-            await db.collection("users").updateOne({name: userHeader},
-              {$set: {
-                lastStatus: now
-              }})
-            break;
-          } catch (e) {
-            console.error(e);
-          }
+          await db.collection("users").updateOne(
+            { name: userHeader },
+            {
+              $set: {
+                lastStatus: now,
+              },
+            }
+          );
+          break;
         }
       }
       res.sendStatus(200);
+      
       return;
     }
   } catch (e) {
     console.error(e);
-    return
+    
+    return;
   }
 });
 
 setInterval(async () => {
-  const now = Date.now()
+  const now = Date.now();
 
   try {
+    
     const users = await db.collection("users").find({}).toArray();
     const disconnectedUsers = users.filter((user) => {
       if (now - user.lastStatus > 10000) return user;
     });
-    try {
-      disconnectedUsers.map(async (user) => {
-        await db.collection("users").deleteOne(user);
-        try {
-          await db
-          .collection("messages")
-          .insertOne({
-            from: user.name,
-            to: "Todos",
-            text: "sai da sala...",
-            type: "status",
-            time: dayjs(now).format("HH:mm:ss"),
-          });
-          return
-        } catch(e) {
-          console.error(e);
-          return
-        }
+    disconnectedUsers.map(async (user) => {
+      await db.collection("users").deleteOne(user);
+      await db.collection("messages").insertOne({
+        from: user.name,
+        to: "Todos",
+        text: "sai da sala...",
+        type: "status",
+        time: dayjs(now).format("HH:mm:ss"),
       });
-    } catch (e) {
-      console.error(e);
-      return
-    }
+      
+      return;
+    });
   } catch (e) {
+    
     console.error(e);
-    return
+    return;
   }
 }, 15000);
 
